68edc911d8cc31685eba3751d1db39865177052a
ksenia <ksy184694@gmail.com>
2023-05-03 21:14:45 +0300

    setting error

--- src/test/java/ru/netology/web/BankCardTest.java
+++ src/test/java/ru/netology/web/BankCardTest.java
@@ -1,6 +1,7 @@
 
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,8 +20,8 @@ public class BankCardTest {
     @BeforeAll
     static void setupClass() {
 
-//        WebDriverManager.chromedriver().setup();
-       System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+//       System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 
     }
 
